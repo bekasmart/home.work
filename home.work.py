9ecabe1a909e3a05ef18303a471bbd424a4143e0
bekasmart <149644433+bekasmart@users.noreply.github.com>
2023-11-09 07:55:11 +0600

    home.work

---
+++ home.work.py
@@ -0,0 +1,165 @@
+# > Beka: 
+# LEVEL 1 
+# task 1 
+word = list () 
+empty_list = list () 
+# print(len(empty_list)) 
+print(empty_list) 
+ 
+ 
+# task 2 
+siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+print(siti) 
+ 
+ 
+# task 3 
+siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+print(len(siti)) 
+ 
+ 
+# task 4 
+siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+first_country = siti [0] 
+print (first_country) 
+third_country = siti [2] 
+print (third_country) 
+fifth_country = siti [4] 
+print (fifth_country) 
+ 
+ 
+# task 5 
+mixed_data_types = {'name':'Malika','age':'17','height':'162','marital status':'single','address':'Turkebaeva 59'} 
+print(mixed_data_types) 
+ 
+ 
+# task 6 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+print(it_companies) 
+ 
+ 
+# task 7 
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+print(fruits) 
+ 
+ 
+# task 8 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+print(len(it_companies)) 
+ 
+ 
+# task 9 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+first_company = it_companies [0] 
+print(first_company) 
+forth_company = it_companies [3] 
+print(forth_company) 
+seventh_company = it_companies [6] 
+print(seventh_company) 
+ 
+ 
+# task 10 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies [-1] = 'Alphabet'  
+print(it_companies) 
+ 
+ 
+# task 11 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies [0] = 'Alphabet' 
+print (it_companies) 
+ 
+ 
+# task 12 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies [4] = 'Alphabet' 
+print (it_companies) 
+  
+  
+# task 13? 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+print(it_companies.upper()) 
+ 
+ 
+#task 14 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+ 
+ 
+# task 15 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+does_exist = 'Facebook' in it_companies  
+print(does_exist) 
+ 
+ 
+# task 16 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.sort() 
+print(it_companies) 
+ 
+ 
+# task 17 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.reverse() 
+print(it_companies) 
+ 
+ 
+# task 18 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+first_company = it_companies[3:] 
+print(first_company) 
+ 
+ 
+# task 19 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+first_company = it_companies[:-3] 
+print(first_company) 
+ 
+ 
+# task 20 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Apple') 
+print(it_companies) 
+ 
+ 
+# task 21 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Facebook') 
+print(it_companies) 
+ 
+ 
+# task 22 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Apple') 
+print(it_companies) 
+ 
+ 
+# task 23 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('Amazon') 
+print(it_companies) 
+ 
+ 
+# task 24? 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+it_companies.remove('it_companies') 
+print(it_companies) 
+ 
+lst = ['item1', 'item2'] 
+lst.pop() 
+ 
+ 
+# task 25 
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+ 
+ 
+ 
+# task 26 
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+back_end = ['Node','Express', 'MongoDB'] 
+integers = front_end + back_end 
+print(integers) 
+ 
+ 
+# task 27? 
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+back_end = ['Node','Express', 'MongoDB'] 
+integers = front_end + back_end
